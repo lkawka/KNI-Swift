4d7d4fff4f58b3728ccf90d3d400b537dfa0bc3c
lkawka <luk.kawka@gmail.com>
2018-03-10 09:18:14 +0100

    Part 2 of UIKit Presentation added

---
+++ pierwsza_apka/Kornel/SimpleCalories/build/Pods.build/Debug-iphonesimulator/GTProgressBar.build/DerivedSources/GTProgressBar_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char GTProgressBarVersionString[];
+ extern const double GTProgressBarVersionNumber;
+
+ const unsigned char GTProgressBarVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:GTProgressBar  PROJECT:Pods-1" "\n";
+ const double GTProgressBarVersionNumber __attribute__ ((used)) = (double)1.;
